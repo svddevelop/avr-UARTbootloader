0d4192e4b8407100f893c08c76d6de2afeab48d8
svddevelop <svddevelop@gmail.com>
2015-04-26 11:05:57 +0200

    Initial commit

---
+++ dep/uartbootloader.o.d
@@ -0,0 +1,55 @@
+uartbootloader.o: ../uartbootloader.c \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/iom8.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/boot.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h \
+  f:\workspaces\uc\atmel\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  f:\workspaces\uc\atmel\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include-fixed/limits.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/iom8.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/boot.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h:
+
+f:\workspaces\uc\atmel\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+f:\workspaces\uc\atmel\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include-fixed/limits.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+f:/workspaces/uc/atmel/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
